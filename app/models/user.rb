e3f2b4df92e9f0c4178b06c7843a8786241ab756
Angel Barros <albarros1973@gmail.com>
2020-12-16 10:43:50 +0100

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :username, :password, presence: true
-  validates :username, uniqueness: {message: 'An account associated with %{value} already exists'}
-  validates :password, length: {in: 4..12}
+  validates :username, uniqueness: { message: 'An account associated with %<value>s already exists' }
+  validates :password, length: { in: 4..12 }
   has_many :posts, dependent: :destroy
 end
